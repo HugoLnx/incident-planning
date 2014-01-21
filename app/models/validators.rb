c65d544b36d44acfcabd930069a1126322f23442
Hugo Roque <hugolnx@gmail.com>
2014-01-21 15:03:34 -0200

    creating a validator for datetime text format

---
+++ app/models/validators.rb
@@ -0,0 +1,5 @@
+=begin
+  Esse arquivo é necessário, pois as constantes dentro do módulo Validators
+  não estavam sendo carregadas automáticamente.
+=end
+require "./app/models/validators/datetime_format_validator.rb"
