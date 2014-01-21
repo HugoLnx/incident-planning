5adcb298b5dbf1783d2d383da80be4271b9e8c85
Hugo Roque <hugolnx@gmail.com>
2014-01-21 18:33:58 -0200

    putting restrictions on cycles and incidents migrations

--- db/migrate/20140117144021_create_incidents.rb
+++ db/migrate/20140117144021_create_incidents.rb
@@ -1,7 +1,7 @@
 class CreateIncidents < ActiveRecord::Migration
   def change
     create_table :incidents do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
--- db/migrate/20140117163103_create_cycles.rb
+++ db/migrate/20140117163103_create_cycles.rb
@@ -1,12 +1,12 @@
 class CreateCycles < ActiveRecord::Migration
   def change
     create_table :cycles do |t|
-      t.integer :incident_id
-      t.integer :number
-      t.integer :current_object
-      t.datetime :from
-      t.datetime :to
-      t.boolean :closed
+      t.integer :incident_id, null: false
+      t.integer :number, null: false
+      t.integer :current_object, null: false
+      t.datetime :from, null: false
+      t.datetime :to, null: false
+      t.boolean :closed, default: false
 
       t.timestamps
     end
