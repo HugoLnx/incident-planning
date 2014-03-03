c69873a34017180d6cebb9e0ed40a66d30880ce8
Hugo Roque <hugolnx@gmail.com>
2014-03-03 15:09:25 -0300

    UI changes in incident objectives page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def cycle_closed_name(is_closed)
+    is_closed ? "closed" : "open"
+  end
+
+  def format_date(date)
+    return "" if date.nil?
+    date.strftime "%d/%m/%Y %H:%M"
+  end
 end
--- app/helpers/cycles_helper.rb
+++ app/helpers/cycles_helper.rb
@@ -1,2 +1,9 @@
 module CyclesHelper
+  def objectives_text_input(form)
+    form.input :objectives_text,
+      as: :text,
+      label:"Objectives (One per line)",
+      disabled: @cycle.persisted?,
+      input_html: {cols: 100, rows: 10}
+  end
 end
