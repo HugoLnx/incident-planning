7c5be493ccf302eae89c4e5aa284576913d0b681
Hugo Roque <hugolnx@gmail.com>
2014-03-03 14:05:04 -0300

    strategies controller accepting delete action

--- app/controllers/strategies_controller.rb
+++ app/controllers/strategies_controller.rb
@@ -25,6 +25,16 @@ class StrategiesController < ApplicationController
     head :ok
   end
 
+  def destroy
+    group = Group.includes(:text_expressions).find(params[:id])
+
+    strategy = HighModels::Strategy.new
+    strategy.set_from_group group
+    strategy.destroy
+
+    head :ok
+  end
+
 private
   def set_incident_and_cycle
     @incident = Incident.find params[:incident_id]
