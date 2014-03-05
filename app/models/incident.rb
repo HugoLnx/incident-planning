f140c26c368df31e24311558cb9955127743f921
Hugo Roque <hugolnx@gmail.com>
2014-03-04 22:02:13 -0300

    ui changes in list of incidents

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -2,4 +2,8 @@ class Incident < ActiveRecord::Base
   has_many :cycles
 
   validates :name, presence: true
+
+  def closed?
+    cycles.all?{|cycle| cycle.closed?}
+  end
 end
---
+++ spec/models/incident_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Incident do
+  context "given an instance" do
+    before :each do
+      @incident = build :incident
+    end
+
+    describe "when getting if is closed verifies if all child cycles are closed" do
+      context 'if all cycles are closed' do
+        it "is true" do
+          @incident.cycles = build_list :cycle, 4, closed: true
+          expect(@incident).to be_closed
+        end
+      end
+
+      context "if any cycle isn't closed" do
+        it "is false" do
+          cycles = build_list :cycle, 4, closed: true
+          cycles << build(:cycle, closed: false)
+          @incident.cycles = cycles
+          expect(@incident).to_not be_closed
+        end
+      end
+    end
+  end
+end
