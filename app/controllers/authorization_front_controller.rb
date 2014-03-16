dd30a4fcdedd0a427f69b7252aec50e72481ee95
Hugo Roque <hugolnx@gmail.com>
2014-03-16 15:15:32 -0300

    controlling access authorization

---
+++ app/controllers/authorization_front_controller.rb
@@ -0,0 +1,24 @@
+class AuthorizationFrontController < ActionController::Base
+  before_filter :verify_authorizations
+
+  RESTRICTED_TO = {
+    user: {
+      "strategies" => true,
+      "tactics" => true,
+      "analysis_matrices" => true,
+      "incidents" => true,
+      "cycles" => true
+    }
+  }
+
+private
+  def verify_authorizations
+    RESTRICTED_TO.each do |user_modelname, controllers|
+      controllers.each do |restricted_controller, _|
+        if self.controller_name == restricted_controller
+          send(:"authenticate_#{user_modelname}!")
+        end
+      end
+    end
+  end
+end
