3ad75bc54844c6656c171554291ceb73f9b4d2e0
Hugo Roque <hugolnx@gmail.com>
2014-04-01 09:34:52 -0300

    SPEC: Changing show metadata event in test to click, because hold can't be simulated with capybara

--- app/assets/javascripts/analysis_matrices/analysis_matrix/actions/show_metadata.js
+++ app/assets/javascripts/analysis_matrices/analysis_matrix/actions/show_metadata.js
@@ -1,12 +1,19 @@
 (function($, namespace) {
   var Actions = namespace.AnalysisMatrix.Actions;
+  var ENVIRONMENT = namespace.FROM_RAILS.environment;
 
   Actions.ShowMetadata = function() {
     this.bindIn = function(_$father, _expression_model_name) {
       var _expression_model_prettyname = _expression_model_name.toLowerCase();
       var selector = "." + _expression_model_prettyname + ".non-repeated";
 
-      $(_$father).onHold(selector, function(event) {
+      if (ENVIRONMENT === "test") {
+        _$father.on("click", selector, displayMetadata);
+      } else {
+        _$father.onHold(selector, displayMetadata);
+      }
+
+      function displayMetadata(event) {
         $(this).find(".metadata").clone().dialog({
           title: _expression_model_name,
           hide: true,
@@ -19,7 +26,7 @@
             $(this).remove();
           }
         });
-      });
+      }
     };
   };
 
