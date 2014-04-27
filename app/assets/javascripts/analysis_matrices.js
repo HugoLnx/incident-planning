4e567d78e231400fa1d06cdbe46fa08c92cb6d44
Hugo Roque <hugolnx@gmail.com>
2014-04-26 23:44:21 -0300

    minor feature: Tactics Matrix - include cycle information and link to tactics matrix page

--- app/assets/javascripts/analysis_matrices.js
+++ app/assets/javascripts/analysis_matrices.js
@@ -2,10 +2,13 @@
 //= require jquery.ui.dialog
 //= require array_utils
 //= require on_hold
+//= require show_dialog_button
 //= require_tree ./ajax/
 
 var matrix;
 $(function(){
   var AnalysisMatrixStarter = LNX_INCIDENT_PLANNING.AnalysisMatrix.AnalysisMatrixStarter;
   matrix = new AnalysisMatrixStarter().start($(".analysis-matrix"));
+
+  $(".btn-show-dialog").asShowDialogButton();
 });
---
+++ app/assets/javascripts/show_dialog_button.js
@@ -0,0 +1,35 @@
+(function($) {
+  $.fn.asShowDialogButton = function() {
+    var $btns = this;
+
+    $btns.show();
+    $(".dialog-title").hide();
+    $btns.each(function() {
+      var dialogId = $(this).data("dialog-id");
+      var $dialogElement = $(document.getElementById(dialogId));
+      $dialogElement.hide();
+    });
+
+    $btns.on("click", function(event) {
+      event.preventDefault();
+
+      var $btn = $(this);
+      var dialogId = $btn.data("dialog-id");
+
+      var $dialogElement = $(document.getElementById(dialogId));
+      var $titleElement = $dialogElement.siblings(".dialog-title");
+      if ($titleElement.length !== 0) {
+        $dialogElement.data("dialog-title", $titleElement.text());
+      }
+      $dialogElement.dialog({
+          title: $dialogElement.data("dialog-title"),
+          hide: true,
+          position: {
+            my: "left top",
+            at: "left bottom",
+            of: $btn
+          }
+      });
+    });
+  }
+} (jQuery));
