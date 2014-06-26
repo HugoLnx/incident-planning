efc610700842a7e7fb7d69075b15b1a8b2255b88
Hugo Roque <hugolnx@gmail.com>
2014-06-26 08:51:48 -0300

    refactoring: encapsulating analysis matrix logic

--- app/controllers/analysis_matrices_controller.rb
+++ app/controllers/analysis_matrices_controller.rb
@@ -2,23 +2,11 @@ class AnalysisMatricesController < ApplicationController
   before_filter :set_incident_and_cycle
 
   def show
-    dao = Dao::AnalysisMatrixDao.new(@cycle)
-    objectives = dao.find_all_objectives_including_hierarchy
-    @matrix_data = AnalysisMatrixData.new(objectives)
-
-    @objective = ::Model.objective
-    @strategy = ::Model.strategy
-    @tactic = ::Model.tactic
+    prepare_to_render_analysis_matrix
   end
 
   def group_approval
-    dao = Dao::AnalysisMatrixDao.new(@cycle)
-    objectives = dao.find_all_objectives_including_hierarchy
-    @matrix_data = AnalysisMatrixData.new(objectives)
-
-    @objective = ::Model.objective
-    @strategy = ::Model.strategy
-    @tactic = ::Model.tactic
+    prepare_to_render_analysis_matrix
 
     @submit_path = group_approval_path
 
@@ -29,5 +17,15 @@ private
   def set_incident_and_cycle
     @incident = Incident.find params[:incident_id]
     @cycle = Cycle.find params[:cycle_id]
+  end
+
+  def prepare_to_render_analysis_matrix
+    dao = Dao::AnalysisMatrixDao.new(@cycle)
+    objectives = dao.find_all_objectives_including_hierarchy
+    @matrix_data = AnalysisMatrixData.new(objectives)
+
+    @objective = ::Model.objective
+    @strategy = ::Model.strategy
+    @tactic = ::Model.tactic
   end
 end
