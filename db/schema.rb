4979d60159eeab3f7a99ed48e2a7593107b83856
Hugo Roque <hugolnx@gmail.com>
2014-07-13 17:43:46 -0300

    updating schema (I'm not sure why that happened when I reset database, may means that the migrations wasn't sync with schema)

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,17 @@
 
 ActiveRecord::Schema.define(version: 20140520104951) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "approvals", force: true do |t|
     t.integer  "expression_id"
     t.string   "expression_type"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "positive",        null: false
-    t.integer  "user_id"
-    t.integer  "role_id"
+    t.integer  "user_id",         null: false
+    t.integer  "role_id",         null: false
   end
 
   add_index "approvals", ["expression_id"], name: "index_approvals_on_expression_id", using: :btree
@@ -69,7 +72,7 @@ ActiveRecord::Schema.define(version: 20140520104951) do
     t.integer "user_id",                               null: false
   end
 
-  add_index "reuse_configurations", ["user_id"], name: "index_reuse_configurations_on_user_id", using: :btree
+  add_index "reuse_configurations", ["user_id"], name: "index_reuse_configurations_on_user_id", using: :hash
 
   create_table "text_expressions", force: true do |t|
     t.string   "name",                                       null: false
