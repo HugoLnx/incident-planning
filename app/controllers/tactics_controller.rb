071173ea8737570810be3fdf2dc7190485757ec9
Hugo Roque <hugolnx@gmail.com>
2014-07-16 10:41:58 -0300

    bugfix: Tactics Matrix - tactics updating and creation with reuse

--- app/controllers/tactics_controller.rb
+++ app/controllers/tactics_controller.rb
@@ -2,14 +2,9 @@ class TacticsController < ApplicationController
   before_filter :set_incident_and_cycle
 
   def create
-    tactic_params = params[:tactic].permit(:who, :what, :where, :when, :response_action,
-      :who_reused, :what_reused, :where_reused, :when_reused, :response_action_reused, :father_id)
-    tactic_params[:owner] = current_user
-    %i{who_reused what_reused where_reused response_action_reused}.each do |param|
-      tactic_params[param] = TextExpression.find_by_id(tactic_params[param])
-    end
-    tactic_params[:when_reused] = TimeExpression.find_by_id(tactic_params[:when_reused])
+    tactic_params = get_tactic_params(extra_permitted: [:father_id])
 
+    tactic_params[:owner] = current_user
     tactic_params[:cycle_id] = @cycle.id
     
     AnalysisMatrixReuse::ParamsCleaner.clean(tactic_params)
@@ -21,8 +16,7 @@ class TacticsController < ApplicationController
   end
 
   def update
-    new_params = params[:tactic].permit(:who, :what, :where, :when, :response_action,
-      :who_reused, :what_reused, :where_reused, :when_reused, :response_action_reused)
+    new_params = get_tactic_params
 
     AnalysisMatrixReuse::ParamsCleaner.clean(new_params)
 
@@ -50,5 +44,19 @@ private
   def set_incident_and_cycle
     @incident = Incident.find params[:incident_id]
     @cycle = Cycle.find params[:cycle_id]
+  end
+
+  def get_tactic_params(extra_permitted: [])
+    permitted = %i{who what where when response_action
+      who_reused what_reused where_reused when_reused response_action_reused}
+    permitted += extra_permitted
+    tactic_params = params[:tactic].permit(*permitted)
+
+    %i{who_reused what_reused where_reused response_action_reused}.each do |param|
+      tactic_params[param] = TextExpression.find_by_id(tactic_params[param])
+    end
+
+    tactic_params[:when_reused] = TimeExpression.find_by_id(tactic_params[:when_reused])
+    tactic_params
   end
 end
--- spec/controllers/tactics_controller_spec.rb
+++ spec/controllers/tactics_controller_spec.rb
@@ -213,40 +213,256 @@ describe TacticsController do
   end
 
   describe "PUT #update" do
-    context "attribute 'when'" do
-      context "tactic have a 'when'" do
-        before :each do
-          @cycle = create :cycle
-          @group = create :tactic_group,
-            cycle: @cycle,
-            father: create(:strategy_group)
-          @cycle.groups << @group
+    context "without reuse" do
+      let(:old_who) { "Old Who" }
+      let(:old_what) { "Old What" }
+      let(:old_where) { "Old Where" }
+      let(:old_when_date) { DateTime.now.beginning_of_minute }
+      let(:old_when_text) { "" }
+      let(:old_response) { "Old Response" }
+
+      before :each do
+        @cycle = create :cycle
+        @group = create :tactic_group,
+          cycle: @cycle,
+          father: create(:strategy_group)
+        @cycle.groups << @group
+
+        @who_exp = @group.text_expressions.find{
+          |exp| exp.name == ::Model.tactic_who.name}
+        @what_exp = @group.text_expressions.find{
+          |exp| exp.name == ::Model.tactic_what.name}
+        @where_exp = @group.text_expressions.find{
+          |exp| exp.name == ::Model.tactic_where.name}
+        @when_exp = @group.time_expressions.find{
+          |exp| exp.name == ::Model.tactic_when.name}
+        @response_exp = @group.text_expressions.find{
+          |exp| exp.name == ::Model.tactic_response_action.name}
+
+        @who_exp.update_attribute(:text, old_who)
+        @what_exp.update_attribute(:text, old_what)
+        @where_exp.update_attribute(:text, old_where)
+        @when_exp.update(when: old_when_date, text: old_when_text)
+        @response_exp.update_attribute(:text, old_response)
+
+        put :update, {
+          tactic: tactic_params,
+          cycle_id: @cycle.id,
+          incident_id: @cycle.incident.id,
+          id: @group.id
+        }
+      end
+
+      shared_examples :all_expressions_expectations do
+        it "have owner that's current user" do
+          expect(expression.owner).to be == subject.current_user
         end
 
-        let(:valid_params) do
-          {
-            tactic: valid_expression_params,
-            cycle_id: @cycle.id,
-            incident_id: @cycle.incident.id,
-            id: @group.id
+        it "have reused equals false" do
+          expect(expression).to_not be_reused
+        end
+
+        it "have source equals nil" do
+          expect(expression.source).to be == nil
+        end
+      end
+
+      context "updating who" do
+        let(:tactic_params){ {who: "New Who"} }
+        let(:expression){@who_exp.reload; @who_exp}
+
+        it "update text" do
+          expect(expression.text).to be == "New Who"
+        end
+
+        include_examples :all_expressions_expectations
+      end
+
+      context "updating what" do
+        let(:tactic_params){ {what: "New What"} }
+        let(:expression){@what_exp.reload; @what_exp}
+
+        it "update text" do
+          expect(expression.text).to be == "New What"
+        end
+
+        include_examples :all_expressions_expectations
+      end
+
+      context "updating where" do
+        let(:tactic_params){ {where: "New Where"} }
+        let(:expression){@where_exp.reload; @where_exp}
+
+        it "update text" do
+          expect(expression.text).to be == "New Where"
+        end
+
+        include_examples :all_expressions_expectations
+      end
+
+      context "updating when" do
+        let(:tactic_params){ {when: "New When"} }
+        let(:expression){@when_exp.reload; @when_exp}
+
+        it "update text" do
+          expect(expression.text).to be == "New When"
+        end
+
+        it "update when" do
+          expect(expression.when).to be == nil
+        end
+
+        include_examples :all_expressions_expectations
+      end
+
+      context "updating response action" do
+        let(:tactic_params){ {response_action: "New Response"} }
+        let(:expression){@response_exp.reload; @response_exp}
+
+        it "update text" do
+          expect(expression.text).to be == "New Response"
+        end
+
+        include_examples :all_expressions_expectations
+      end
+
+      context "updating when to blank" do
+        let(:tactic_params){ {when: ""} }
+        let(:expression){@when_exp.reload; @when_exp}
+
+        it "update text to blank" do
+          expect(expression.text).to be == ""
+        end
+
+        it "update when to nil" do
+          expect(expression.when).to be == nil
+        end
+
+        include_examples :all_expressions_expectations
+      end
+    end
+
+    context "with reuse" do
+      let(:old_who) { "Old Who" }
+      let(:old_what) { "Old What" }
+      let(:old_where) { "Old Where" }
+      let(:old_when_date) { DateTime.now.beginning_of_minute }
+      let(:old_when_text) { "" }
+      let(:old_response) { "Old Response" }
+
+      let(:reused_who){create(:tactic_who)}
+      let(:reused_what){create(:tactic_what)}
+      let(:reused_where){create(:tactic_where)}
+      let(:reused_when){create(:tactic_when)}
+      let(:reused_response){create(:tactic_response_action)}
+
+      before :each do
+        @cycle = create :cycle
+        @group = create :tactic_group,
+          cycle: @cycle,
+          father: create(:strategy_group)
+        @cycle.groups << @group
+
+        @who_exp = @group.text_expressions.find{
+          |exp| exp.name == ::Model.tactic_who.name}
+        @what_exp = @group.text_expressions.find{
+          |exp| exp.name == ::Model.tactic_what.name}
+        @where_exp = @group.text_expressions.find{
+          |exp| exp.name == ::Model.tactic_where.name}
+        @when_exp = @group.time_expressions.find{
+          |exp| exp.name == ::Model.tactic_when.name}
+        @response_exp = @group.text_expressions.find{
+          |exp| exp.name == ::Model.tactic_response_action.name}
+
+        @who_exp.update_attribute(:text, old_who)
+        @what_exp.update_attribute(:text, old_what)
+        @where_exp.update_attribute(:text, old_where)
+        @when_exp.update(when: old_when_date, text: old_when_text)
+        @response_exp.update_attribute(:text, old_response)
+
+        put :update, {
+          cycle_id: @cycle.id,
+          incident_id: @cycle.incident.id,
+          id: @group.id,
+          tactic: {
+            who_reused: reused_who.id,
+            what_reused: reused_what.id,
+            where_reused: reused_where.id,
+            when_reused: reused_when.id,
+            response_action_reused: reused_response.id,
           }
+        }
+      end
+
+      shared_examples :all_expressions_expectations do
+        it "is not nil" do
+          expect(expression).not_to be nil
         end
 
-        it "update the existing one" do
-          old_exp = @group.time_expressions.first
-          put :update, valid_params
-          @group.reload
-          new_exp = @group.time_expressions.first
-          expect(new_exp).to be == old_exp
-          expect(new_exp.when).to be == valid_date
+        it "have owner that's current user" do
+          expect(expression.owner).to be == subject.current_user
         end
 
-        it "update the text if no value was passed" do
-          put :update, valid_params.merge({tactic: valid_expression_params.merge({when: ""})})
-          time_expression = TimeExpression.first
-          expect(time_expression.when).to be_nil
-          expect(time_expression.text).to be == ""
+        it "have reused equals true" do
+          expect(expression).to be_reused
         end
+
+        it "have source equals of reused expression" do
+          expect(expression.source).to be == reused_expression.source
+        end
+      end
+
+      shared_examples :text_expressions_expectations do
+        it "have text equals of reused expression" do
+          expect(expression.text).to be == reused_expression.text
+        end
+      end
+
+      describe "the when updated" do
+        let(:reused_expression) {reused_when}
+        let(:expression) {@when_exp.reload; @when_exp}
+
+        include_examples :all_expressions_expectations
+
+        it "have when equals of reused expression" do
+          expect(expression.when).to be == reused_expression.when
+        end
+
+        it "have text equals of reused expression" do
+          expect(expression.text).to be == reused_expression.text
+        end
+      end
+
+      describe "the who updated" do
+        let(:reused_expression) {reused_who}
+        let(:expression) {@who_exp.reload; @who_exp}
+
+        include_examples :all_expressions_expectations
+        include_examples :text_expressions_expectations
+      end
+
+      describe "the what updated" do
+        let(:reused_expression) {reused_what}
+        let(:expression) {@what_exp.reload; @what_exp}
+
+        include_examples :all_expressions_expectations
+        include_examples :text_expressions_expectations
+      end
+
+      describe "the where updated" do
+        let(:reused_expression) {reused_where}
+        let(:expression) {@where_exp.reload; @where_exp}
+
+        include_examples :all_expressions_expectations
+        include_examples :text_expressions_expectations
+      end
+
+      describe "the response_action updated" do
+        let(:reused_expression) {reused_response}
+        let(:expression) {@response_exp.reload; @response_exp}
+
+        include_examples :all_expressions_expectations
+        include_examples :text_expressions_expectations
       end
     end
 
