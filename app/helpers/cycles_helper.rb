95133cb3d96d97c253f0282eb92e77ec72195ef8
Hugo Roque <hugolnx@gmail.com>
2014-08-05 11:52:53 -0300

    Incident Objectives: chanaing some terms to be more understandable to Petrobras employees

--- app/helpers/cycles_helper.rb
+++ app/helpers/cycles_helper.rb
@@ -1,9 +1,15 @@
 module CyclesHelper
+  RESOURCE_NAME = "Incident Objectives (ICS 202)"
+
   def objectives_text_input(form, form202)
     form.input :objectives_text,
       as: :text,
       label:"Objectives (One per line)",
       disabled: form202.persisted?,
       input_html: {cols: 100, rows: 10}
+  end
+
+  def resource_name
+    RESOURCE_NAME
   end
 end
