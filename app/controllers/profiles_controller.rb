37bc32f7a7deac25910e989585d2e222cea15774
Hugo Roque <hugolnx@gmail.com>
2014-08-28 16:02:14 -0300

    SearchUsers: removing clear alarm

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,5 @@
 class ProfilesController < ApplicationController
   def index
-    if params[:commit] != "Clear"
-      @filter_term = params[:filter]
-    end
     @users = QueryFilter.new(User.all).filter(@filter_term, by: %w{name email}).load
   end
 end
