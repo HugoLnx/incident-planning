0517c712606c370ac65ab07f3a913fcb70864d6c
Hugo Roque <hugolnx@gmail.com>
2014-08-28 15:58:30 -0300

    bugfix: query filter including users with no email or name

--- app/models/query_filter.rb
+++ app/models/query_filter.rb
@@ -6,10 +6,10 @@ class QueryFilter
   def filter(term, by: [])
     attrs = by
     if QueryUtils.postgresql?
-      attrs.map{|attr| "COALESCE(#{attr}, '')"}
+      attrs.map!{|attr| "COALESCE(#{attr}, '')"}
       concat = "(#{attrs.join(" || ")})"
     else
-      attrs.map{|attr| "IFNULL(#{attr}, '')"}
+      attrs.map!{|attr| "IFNULL(#{attr}, '')"}
       concat = "CONCAT(#{attrs.join(",")})"
     end
     QueryUtils.where_attr_like(@query, concat, term)
