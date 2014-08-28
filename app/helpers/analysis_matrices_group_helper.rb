52fc58791fbee185f7bbe4d5ed3bc7e2a7f67bc2
Hugo Roque <hugolnx@gmail.com>
2014-08-28 15:09:48 -0300

    TacticsMatrix DeletionGroup: hiding checkbox when user can't delete

--- app/helpers/analysis_matrices_group_helper.rb
+++ app/helpers/analysis_matrices_group_helper.rb
@@ -1,23 +1,52 @@
 module AnalysisMatricesGroupHelper
+  def approval_checkbox_for_objective?(row)
+    checkbox_for_objective?(row) &&
+      current_user.can_approve_any_expression_of?(row.objective.objective.group)
+  end
+
+  def approval_checkbox_for_strategy?(row)
+    checkbox_for_strategy?(row) &&
+      current_user.can_approve_any_expression_of?(row.strategy.strategy.group)
+  end
+
+
+  def approval_checkbox_for_tactic?(row)
+    checkbox_for_tactic?(row) &&
+      current_user.can_approve_any_expression_of?(row.tactic.tactic.group)
+  end
+
+  def deletion_checkbox_for_strategy?(row)
+    checkbox_for_strategy?(row) &&
+      can_create_one_of_same_type_of?(row.strategy.strategy.group)
+  end
+
+  def deletion_checkbox_for_tactic?(row)
+    checkbox_for_tactic?(row) &&
+      can_create_one_of_same_type_of?(row.tactic.tactic.group)
+  end
+
+private
+  def can_create_one_of_same_type_of?(group)
+      group_model = ::Model.find_by_group_name(group.name)
+      permission = GroupPermission.new(group_model).to_create?(current_user)
+  end
+
   def checkbox_for_objective?(row)
     row.objective &&
       row.objective.objective &&
-      !row.objective.repeated &&
-      current_user.can_approve_any_expression_of?(row.objective.objective.group)
+      !row.objective.repeated
   end
 
   def checkbox_for_strategy?(row)
     row.strategy.respond_to?(:strategy) &&
       row.strategy.strategy &&
-      !row.strategy.repeated &&
-      current_user.can_approve_any_expression_of?(row.strategy.strategy.group)
+      !row.strategy.repeated
   end
 
 
   def checkbox_for_tactic?(row)
     row.tactic.respond_to?(:tactic) &&
       row.tactic.tactic &&
-      !row.tactic.repeated &&
-      current_user.can_approve_any_expression_of?(row.tactic.tactic.group)
+      !row.tactic.repeated
   end
 end
