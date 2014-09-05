a2cb4061489cb19c88fb7f8edae502ae709c4875
Hugo Roque <hugolnx@gmail.com>
2014-09-05 14:32:41 -0300

    Publishing final version (isn't worring about prepared-by and approved-by)

--- app/controllers/publishes_controller.rb
+++ app/controllers/publishes_controller.rb
@@ -9,7 +9,7 @@ class PublishesController < ApplicationController
     @group_errors = all_messages[:group]
 
     if !Publish::ValidationUtils.have_errors?(all_messages)
-      Publish::Publisher.publish(@cycle)
+      Publish::Publisher.publish(@cycle, ics234_pdf: render_matrix_pdf, ics202_pdf: render_objectives_pdf)
       redirect_to action: :show
     else
       render "analysis_matrices/show"
@@ -35,6 +35,7 @@ private
   def set_incident_and_cycle
     @incident = Incident.find params[:incident_id]
     @cycle = Cycle.find params[:cycle_id]
+    @form202 = Forms::Form202.new_from(@cycle)
   end
 
   def prepare_to_render_analysis_matrix
@@ -45,5 +46,19 @@ private
     @objective = ::Model.objective
     @strategy = ::Model.strategy
     @tactic = ::Model.tactic
+  end
+
+  def render_matrix_pdf
+    @final = true
+    render_to_string pdf: "anything",
+      template: "analysis_matrices/show.pdf.erb",
+      layout: "application"
+  end
+
+  def render_objectives_pdf
+    @final = true
+    render_to_string pdf: "anything",
+      template: "cycles/show.pdf.erb",
+      layout: "application"
   end
 end
--- app/models/publish/publisher.rb
+++ app/models/publish/publisher.rb
@@ -1,9 +1,10 @@
 module Publish
   class Publisher
-    def self.publish(cycle)
+    def self.publish(cycle, ics234_pdf: nil, ics202_pdf: nil)
       incident_name = cycle.incident.name
       cycle.text_expressions.update_all(source: incident_name)
       cycle.time_expressions.update_all(source: incident_name)
+      cycle.last_version.update(ics234_pdf: ics234_pdf, ics202_pdf: ics202_pdf)
       cycle.update(closed: true)
     end
   end
