7f6c80c1f4a6f3a140e394534b18b3d45fe86451
Hugo Roque <hugolnx@gmail.com>
2014-10-15 17:57:40 -0300

    seeds creating admin company

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Company.find_or_create_by(name: "Admin", id: 99999)
+User.where("email in ('hugolnx@gmail.com', 'malencar@tecgraf.puc-rio.br', 'malencar@inf.puc-rio.br', 'marcus.4biz@gmail.com')")
+  .update_all(company_id: 99999)
