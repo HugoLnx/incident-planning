6a889595fffac25253e9347601165806204729a0
Hugo Roque <hugolnx@gmail.com>
2014-10-20 14:34:05 -0200

    no login link and create user don't require authentication

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
+  skip_before_filter :require_no_authentication, only: [:new, :create]
+
 protected
   def after_update_path_for(resource_or_scope)
     from_config_back_path
+  end
+
+  def sign_up(resource_name, resource)
   end
 end
