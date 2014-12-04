ef2af2b2586c93159331e73f8d18876d9864a57c
Hugo Roque <hugolnx@gmail.com>
2014-12-04 12:41:47 -0200

    Styling: Tactics Matrix - flag is positioned right

--- app/assets/javascripts/analysis_matrices.js
+++ app/assets/javascripts/analysis_matrices.js
@@ -22,4 +22,12 @@ $(function(){
   matrix = new AnalysisMatrixStarter().start($(".analysis-matrix"));
 
   $(".btn-show-dialog").asShowDialogButton();
+  $(".approvable-triangle").each(function() {
+    var $triangleDiv = $(this);
+    var $td = $triangleDiv.closest("td");
+    var position = $td.offset()
+    position.top += 2;
+    position.left += 2;
+    $triangleDiv.offset(position);
+  });
 });
